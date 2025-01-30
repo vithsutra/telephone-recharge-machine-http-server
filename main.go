01c84582fa03a08af451c1b515d8c93a670f5c1d
Magowtham <magowtham7@gmail.com>
2025-01-30 23:15:37 +0530

    updated server mode shifting feature from dev to prod based on .env file loading

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/Magowtham/telephone_recharge_machine_http_server/infrastructure"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	mode := os.Getenv("SERVER_MODE")
+
+	if mode == "dev" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("error loading the env file, Error -> %v\n", err.Error())
+			return
+		} else {
+			log.Println(".env file loaded successfully")
+			return
+		}
+	}
+
+	if mode != "prod" {
+		log.Fatalln("please set the SERVER_MODE env variable to 'prod' or 'dev'")
+	}
+}
 
 func main() {
 
 	//intializing the logger file
 	initLogger()
-
-	// if err := godotenv.Load(); err != nil {
-	// 	log.Fatalf("error loading the env file, Error -> %v\n", err.Error())
-	// }
 
 	infrastructure.Run()
 }
