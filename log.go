1e7b1de82e0927dbe469c6d41ecc4fd7c6d4ce35
sathwik kd <skadasur@gmail.com>
2025-02-27 15:34:48 +0530

    logs updated

--- log.go
+++ log.go
@@ -7,16 +7,15 @@ import (
 )
 
 func initLogger() {
-	logFileName := "app.log"
+	logFilePath := "/var/logs/app.log" // Ensure it matches your Docker volume
 
-	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("unable to create logger file, Error -> %v\n", err.Error())
 	}
 
 	logWriter := io.MultiWriter(os.Stdout, logFile)
-
 	log.SetOutput(logWriter)
 
+	log.Println("Logger initialized successfully!")
 }
